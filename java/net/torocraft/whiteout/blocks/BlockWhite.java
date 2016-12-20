857d2f13d41fcc01a3271383d6f8f2a67a09bf17
Charles Howard <cdhoward@add123.com>
2016-12-19 20:08:37 -0500

    removing shading

--- java/net/torocraft/whiteout/blocks/BlockWhite.java
+++ java/net/torocraft/whiteout/blocks/BlockWhite.java
@@ -3,13 +3,19 @@ package net.torocraft.whiteout.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.torocraft.whiteout.Whiteout;
 
 public class BlockWhite extends Block {
@@ -29,6 +35,17 @@ public class BlockWhite extends Block {
 		ITEM_INSTANCE = new ItemBlock(INSTANCE);
 		ITEM_INSTANCE.setRegistryName(resourceName);
 		GameRegistry.register(ITEM_INSTANCE);
+	}
+
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	@Override
+	public int getLightOpacity(IBlockState state, IBlockAccess world, BlockPos pos) {
+		// TODO Auto-generated method stub
+		return super.getLightOpacity(state, world, pos);
 	}
 
 	public static void registerRenders() {
